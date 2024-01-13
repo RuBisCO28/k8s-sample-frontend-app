c84544599eee9fec0628f8ed9ebb06f4d97e121d
RuBiCO28 <54470624+RuBisCO28@users.noreply.github.com>
2024-01-13 19:28:19 +0900

    change to use nitro

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  vite: {
-    server: {
-      proxy: {
-        '/api': {
-          target: 'http://sample-backend-app-service:8080',
-          changeOrigin: true,
-          // rewrite: (path) => path.replace(/^\/api/, ''),
-        }
-    },
+  nitro: {
+    routeRules: {
+      '/api/**': {
+        proxy: process.env.BASE_API_URL
+          ? `${process.env.BASE_API_URL}/**`
+          : 'http://localhost:8080/api/**'
+      },
+    }
   },
-}
 })
